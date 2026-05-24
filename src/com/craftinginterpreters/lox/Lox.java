73cd10f25238cb392327c682f152ecfa0cd502e9
YLL-0 <tilendv@gmail.com>
2026-05-24 17:38:11 +0200

    scanner done, it displays all  the info when enterd

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -7,61 +7,59 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
-
-
 
 // var language = "lox";
 // when we scan through the list of chars. and group them together into sequences we get a LEXEME.
 // Lexems together form a token
 
-
-
 public class Lox {
 
- static boolean hadError = false;
+    static boolean hadError = false;
 
-
-// reads a file and executes it
-private static void runFile(String path) throws IOException {
-    byte[] bytes = Files.readAllBytes(Paths.get(path));
-    run(new String(bytes, Charset.defaultCharset()));
-}
-
-// interpreter prompt is called a REPL {Read a line imput, Evalate it, Print the resault then Loop everything}
-private static void runPrompt() throws IOException{
-    InputStreamReader input = new InputStreamReader(System.in);
-    BufferedReader reader = new BufferedReader(input);
-
-    for (;;){
-        System.out.println("> ");
-        String line = reader.readLine();
-        if (line == null) break;
-        run(line);
+    // reads a file and executes it
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
     }
-}
 
-// printing tokens the scanner wil emit
-private static void run(String source){
-    Scanner scanner = new Scanner(source);
-    List<Token> tokens = scanner.scanTokns();
+    // interpreter prompt is called a REPL {Read a line imput, Evalate it, Print the
+    // resault then Loop everything}
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
 
-    for (Token token : tokens){
-        System.out.println(token);
+        for (;;) {
+            System.out.println("> ");
+            String line = reader.readLine();
+            if (line == null)
+                break;
+            run(line);
+        }
     }
-}
-// error and report tell some syntax error occurred on a given line (this is bare minimum)
-static void error(int line, String message){
-    report(line, " ", message);
-}
 
-// better to knew where it happened and not "There is an error gl hf"
-private static void report(int line, String where, String message){
-    System.err.println("[line " + "] Error" + where + ": "+ message);
-    hadError = true;
-}
+    // printing tokens the scanner will emit
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
 
-     static void main(String[] args) throws IOException {
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    // error and report tell some syntax error occurred on a given line (this is
+    // bare minimum)
+    static void error(int line, String message) {
+        report(line, " ", message);
+    }
+
+    // better to knew where it happened and not "There is an error gl hf"
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+
+    static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
             System.exit(64);
--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -1,10 +1,13 @@
 package com.craftinginterpreters.lox;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.craftinginterpreters.lox.TokenType.*;
 
-public class Scanner {
+class Scanner {
 
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
@@ -12,84 +15,238 @@ public class Scanner {
     private int current = 0;
     private int line = 1;
 
+    // hash map shranjenin identifierjev za vse pomembne keywoarde
+    private static final Map<String, TokenType> keywords;
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
+
     // start point at the first char in the lexeme being scanned,
-    // current points at the cahr currently being considered
+    // current points at the char currently being considered
     // line tracks what source line current is on
 
-
-    Scanner(String source){
+    Scanner(String source) {
         this.source = source;
     }
 
-    List<Token> scanTokens(){
-       while (!isAtEnd()){
-           // we are at the beginning of the next lexeme
-           start = current;
-           scanTokens();
-       }
-       tokens.add(new Token(EOF,"",null,line));
-       return tokens;
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            // We are at the beginning of the next lexeme.
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
     }
 
-    private void scanToken(){
+    private void scanToken() {
         char c = advance();
-        switch (c){
-            case '(': addToken(LEFT_PAREN); break;
-            case ')': addToken(RIGHT_PAREN); break;
-            case '{': addToken(LEFT_BRACE); break;
-            case '}': addToken(RIGHT_BRACE); break;
-            case ',': addToken(COMMA); break;
-            case '.': addToken(DOT); break;
-            case '-': addToken(MINUS); break;
-            case '+': addToken(PLUS); break;
-            case ';': addToken(SEMICOLON); break;
-            case '*': addToken(STAR); break;
-            // because =, !, <, > can have a following statement(!=,<=, =>...) we handle them a bit different
+        switch (c) {
+            case '(':
+                addToken(LEFT_PAREN);
+                break;
+            case ')':
+                addToken(RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(LEFT_BRACE);
+                break;
+            case '}':
+                addToken(RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(COMMA);
+                break;
+            case '.':
+                addToken(DOT);
+                break;
+            case '-':
+                addToken(MINUS);
+                break;
+            case '+':
+                addToken(PLUS);
+                break;
+            case ';':
+                addToken(SEMICOLON);
+                break;
+            case '*':
+                addToken(STAR);
+                break;
+            // because =, !, <, > can have a following statement(!=,<=, =>...) we handle
+            // them a bit different
             // that's why we need to look for the second character
-            case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
-            case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
-            case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
-            case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
             // if Lox doesn't support a char it prints it as an error
-            default: Lox.error(line, "Unexpected character"); break;
+            case '/':
+                // preveri ce je komentar do konca vrstice
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd())
+                        advance();
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+            // celoten point tega if je da iscemo druki / in gre do konca vrstice da vemo
+            // ali je komenrat ali samo /
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            // ignore whitespace, ce je whitespace skoci na zacetek scan loopa
+            case '\n':
+                line++;
+                break;
+
+            case '"':
+                string();
+                break;
+            default:
+                if (isDigit(c)) {
+                    number();
+                } else if (isAplha(c)) {
+                    identifier();
+                } else {
+                    Lox.error(line, "Unexpected character");
+                }
         }
     }
 
+    private void identifier() {
+        while (isAlphaNumeric(peek()))
+            advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null)
+            type = IDENTIFIER;
+        addToken(type);
+
+    }
+
+    // iscemo '.' ker imamo stevilke shranjene kot 123.0, torej . nam pvoe da je za
+    // njo samo se ena stevilka
+    private void number() {
+        while (isDigit(peek()))
+            advance();
+
+        // gledamo za plavajoco vejicondro
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+            while (isDigit(peek()))
+                advance();
+        }
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    // po prvem " preverja in shranjuje sting dokler ne njade ending "
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n')
+                line++;
+            advance();
+        }
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+        // zaperanje
+        advance();
+
+        // trim the surrounding quotes
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+
+    }
 
     // like advance() we only consume the current char if It's what were looking for
-    // basically we check if the lexems in to stages and if we jump to the second one
-    private boolean match(char expected){
-        if (isAtEnd())  return false;
-        if (source.charAt(current) != expected) return false;
+    // basically we check if the lexems in to stages and if we jump to the second
+    // one
+    private boolean match(char expected) {
+        if (isAtEnd())
+            return false;
+        if (source.charAt(current) != expected)
+            return false;
 
         current++;
         return true;
     }
 
-
-
-    private boolean isAtEnd(){
-        return  current >= source.length();
+    // 'lokahead' pomaga nam videti kaj je nasledni char
+    private char peek() {
+        if (isAtEnd())
+            return '\0';
+        return source.charAt(current);
     }
 
+    // pogelda ce obstarja nasledni po peek in ce ga vrne
+    private char peekNext() {
+        if (current + 1 >= source.length())
+            return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isAplha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAplha(c) || isDigit(c);
+    }
+
+    // ko vemo ali je stevilka ga ra razdelimo na dele kakor pri string
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
 
     // in the input it consumes the next character in the source file and returns it
-    private char advance(){
+    private char advance() {
         return source.charAt(current++);
     }
 
-
     // it creates a Token form the lexeme
-    private void addToken(TokenType type){
-        addToken(type,null);
+    private void addToken(TokenType type) {
+        addToken(type, null);
     }
 
-    private void addToken(TokenType type, Object lineteral){
+    private void addToken(TokenType type, Object literal) {
         String text = source.substring(start, current);
-        tokens.add(new Token(type,text,literal,line));
+        tokens.add(new Token(type, text, literal, line));
 
     }
-
-
+    // Maximal munch
 
 }
